22b39b25808233e50a22a464a103b2c8fb421a8b
Shenal Akalanka <shenalakalanka513@gmail.com>
2022-06-02 11:53:58 +0530

    Initial commit

---
+++ setupScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture_Seri
+{
+    public partial class setupScreen : Form
+    {
+        public setupScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
